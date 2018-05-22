670597d8695ae3185f743ebbce1efece1e6f402e
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2018-05-22 08:22:46 +0100

    new html export mechanism

--- web/XChemWeb.py
+++ web/XChemWeb.py
@@ -180,7 +180,7 @@ class export_to_html:
         os.chdir(os.path.join(self.htmlDir, 'pdbs'))
         self.pdb = None
         if os.path.isfile(os.path.join(self.projectDir,xtal,'refine.split.bound-state.pdb')):
-            self.pdb = pdbtools('refine.split.bound-state.pdb')
+            self.pdb = pdbtools(os.path.join(self.projectDir,xtal,'refine.split.bound-state.pdb'))
             self.Logfile.insert('%s: copying refine.split.bound-state.pdb to html directory' %xtal)
             os.system('/bin/cp %s/refine.split.bound-state.pdb %s.pdb' %(os.path.join(self.projectDir,xtal),xtal))
         else:
