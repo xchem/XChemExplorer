5753f11e8bb4be3077b8629d03bdf5aeda6cf622
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2018-06-01 12:25:23 +0100

    new html preparation

--- lib/XChemMain.py
+++ lib/XChemMain.py
@@ -662,7 +662,47 @@ def html_header():
         '</head>\n'
         '<body>\n'
         '    <script src="https://unpkg.com/ngl"></script>\n'
-        '    <script src="js/create_view.js"></script>\n'
+        '    <script >'+"""
+        
+            function create_view(div_name,pdb_bound,event_name,lig_name) {
+    // Create NGL Stage object
+    var stage = new NGL.Stage(div_name);
+    // Handle window resizing
+    window.addEventListener( "resize", function( event ){
+        stage.handleResize();
+    }, false );
+    // Code for example: test/map-shift
+    Promise.all( [
+        //stage.loadFile( "data://"+event_name+".ccp4" ),
+        //stage.loadFile( "data://"+event_name+".pdb" )
+        stage.loadFile( event_name ),
+        stage.loadFile( pdb_bound ),
+    
+    ] ).then( function( ol ){
+        var map = ol[ 0 ];
+        var struc = ol[ 1 ];
+        struc.autoView(lig_name)
+        var surfRepr = map.addRepresentation( "surface", {
+            boxSize: 10,
+            useWorker: false,
+            wrap: true,
+            color: "skyblue",
+            contour: true
+        } );
+        struc.addRepresentation( "cartoon" );
+        struc.addRepresentation( "ball+stick", { sele: "hetero" } );
+        stage.setFocus( 95 );
+        stage.mouseObserver.signals.scrolled.add( function( delta ){
+            if( stage.mouseObserver.altKey ){
+                var d = Math.sign( delta ) / 5;
+                var l = surfRepr.getParameters().isolevel;
+                surfRepr.setParameters( { isolevel: l + d } );
+            }
+        } );
+    } );
+    };
+        """+
+        '</script>\n'
         '\n'
         '    <div id="viewport" style="width:500px;height:500px"></div>\n'
         '\n'
