6cbe559c818e7f24f5c1b27d7b92c15ec6946937
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2020-01-23 09:04:06 +0000

    v1.4.6 (21/01/2020) - added parsing of DIALS JSON merging statistics file and MMCIF cell parameter file

--- lib/XChemUtils.py
+++ lib/XChemUtils.py
@@ -798,6 +798,9 @@ class parse:
 #        self.aimless['DataProcessingScore'] =
         json_name = logfile[logfile.rfind('/')+1:]
         mmcif_file = logfile.replace('LogFiles','DataFiles').replace(json_name,'xia2.mmcif')
+        print '>>>>>>>>>> json: ',json_name
+        print '>>>> looking for:',mmcif_file
+        quit()
         if os.path.isfile(mmcif_file):
             self.read_mmcif(mmcif_file)
 
