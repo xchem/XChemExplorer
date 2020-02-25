e0e5810b450d2ad8c493596158cb9daf1b10cf27
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2020-02-25 14:51:07 +0000

    v1.5.2 (25/02/2020) - bug fix: correct determination of staraniso runs for different space groups

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -2211,6 +2211,8 @@ class choose_autoprocessing_outcome(QtCore.QThread):
         self.agamemnon = agamemnon
         self.xce_logfile = xce_logfile
         self.Logfile = XChemLog.updateLog(xce_logfile)
+        print '>>>>>>>>>>>',self.selection_mechanism
+        quit()
 
         self.db = XChemDB.data_source(os.path.join(database))
         if self.agamemnon:
