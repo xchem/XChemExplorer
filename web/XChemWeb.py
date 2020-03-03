733c78551d4ad9f9d3e05b774fd303baadf5d32d
Tobias Krojer <tobias.krojer@sgc.ox.ac.uk>
2020-03-03 11:43:36 +0000

    v1.5.7 (03/03/2020) - bug fix: capture all available event maps during html export

--- web/XChemWeb.py
+++ web/XChemWeb.py
@@ -279,11 +279,11 @@ class export_to_html:
         if eventMAP == '' or 'none' in str(eventMAP).lower():
             self.Logfile.warning('%s: the respective field in the DB is apparently emtpy' %xtal)
             self.Logfile.warning('%s: will try to determine ligand - event map relationship by checking CC...' %xtal)
-            eventMap = self.find_matching_event_map(xtal,ligID)
+            eventMAP = self.find_matching_event_map(xtal,ligID)
         elif not os.path.isfile(eventMAP):
             self.Logfile.warning('%s: event map file does not exist!' %xtal)
             self.Logfile.warning('%s: will try to determine ligand - event map relationship by checking CC...' %xtal)
-            eventMap = self.find_matching_event_map(xtal,ligID)
+            eventMAP = self.find_matching_event_map(xtal,ligID)
         else:
             self.Logfile.insert('%s: found matching event map!' %xtal)
         return eventMAP
