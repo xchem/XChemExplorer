269415bed1754f3856ffe9f50ba56194c3f6b47d
Tobias Krojer <tobias.krojer@sgc.ox.ac.uk>
2020-07-21 15:13:34 +0100

    v1.7.5 (21/07/2020) - bug fix: remove obsolete 'module load mx' statement from xce_<restraints_prg> script which currently loads a gemmi incompatible ccp4 version

--- lib/XChemUtils.py
+++ lib/XChemUtils.py
@@ -274,9 +274,9 @@ class helpers:
 
         strip_hydrogens = 'phenix.reduce {0!s}.pdb -trim > {0!s}_tmp.pdb'.format(compoundID.replace(' ', ''))
 
-        module = ''
-        if os.path.isdir('/dls'):
-            module = 'module load mx\n'
+#        module = ''
+#        if os.path.isdir('/dls'):
+#            module = 'module load mx\n'
 
         Cmds = (
 
@@ -284,9 +284,9 @@ class helpers:
             '\n'
             'export XChemExplorer_DIR="' + os.getenv('XChemExplorer_DIR') + '"' +
             '\n'
-            'source $XChemExplorer_DIR/setup-scripts/xce.setup-sh'
-            '\n'
-            + module +
+#            'source $XChemExplorer_DIR/setup-scripts/xce.setup-sh'
+#            '\n'
+#            + module +
             '\n'
             '$CCP4/bin/ccp4-python $XChemExplorer_DIR/helpers/update_status_flag.py {0!s} {1!s} {2!s} {3!s}'
             .format(os.path.join(database_directory, data_source_file), sample, 'RefinementCIFStatus', 'running') +
