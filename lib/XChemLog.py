efc153bad94502bcb3049abca954ca051237b009
Tobias Krojer <tobias.krojer@sgc.ox.ac.uk>
2020-11-11 15:22:20 +0000

    v1.7.25 (11/11/2020) (#387)
    - convert json logfile from dials into pseudo aimless file for mmcif file preparation (required for pdb-extract)

--- lib/XChemLog.py
+++ lib/XChemLog.py
@@ -26,7 +26,7 @@ class startLog:
             '     #                                                                     #\n'
             '     # Version: %s                                       #\n' %pasteVersion+
             '     #                                                                     #\n'
-            '     # Date: 03/11/2020                                                    #\n'
+            '     # Date: 11/11/2020                                                    #\n'
             '     #                                                                     #\n'
             '     # Authors: Tobias Krojer, Structural Genomics Consortium, Oxford, UK  #\n'
             '     #          tobias.krojer@sgc.ox.ac.uk                                 #\n'
