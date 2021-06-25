cd101e7a4f95e62ddf643b9dcce50a7fa43707b1
Tobias Krojer <tobkro@Tobiass-Air.lan>
2021-06-26 00:13:04 +0200

    v1.7.34 (25/06/2021) - added pdbx_audit_support category for funding agency now required for PDB deposition

--- lib/XChemDeposit.py
+++ lib/XChemDeposit.py
@@ -310,15 +310,6 @@ class templates:
             "_pdbx_struct_assembly_depositor_info.method_details       PISA\n"
             '_pdbx_struct_assembly_depositor_info.oligomeric_count     %s\n'        %depositDict['biological_assembly_chain_number']+
             '#\n'            
-            'loop\n'
-            '_pdbx_audit_support.ordinal                   \n'
-            '_pdbx_audit_support.funding_organization      \n'
-            '_pdbx_audit_support.grant_number              \n'
-            '_pdbx_audit_support.country                   \n'
-            '%s %s %s %s\n' %(depositDict['pdbx_funding_ordinal_one'],depositDict['pdbx_funding_organization_one'],depositDict['pdbx_grant_number_one'],depositDict['pdbx_grant_country_one']) +
-            '%s %s %s %s\n' %(depositDict['pdbx_funding_ordinal_two'],depositDict['pdbx_funding_organization_two'],depositDict['pdbx_grant_number_two'],depositDict['pdbx_grant_country_two']) +
-            '%s %s %s %s\n' %(depositDict['pdbx_funding_ordinal_three'],depositDict['pdbx_funding_organization_three'],depositDict['pdbx_grant_number_three'],depositDict['pdbx_grant_country_three']) +
-            '#\n'
             )
 
         return data_template_cif
@@ -458,6 +449,8 @@ class prepare_mmcif_files_for_deposition(QtCore.QThread):
                 if not self.create_model_mmcif(xtal):
                     continue
 
+                self.add_funding_information(xtal)
+
 #                if not self.create_sf_mmcif(xtal):
 #                    continue
 
@@ -509,6 +502,8 @@ class prepare_mmcif_files_for_deposition(QtCore.QThread):
 
                 if not self.create_model_mmcif(xtal):
                     continue
+
+                self.add_funding_information(xtal)
 
                 if not self.add_ligand_cif_to_model_mmcif(xtal):
                     continue
@@ -928,27 +923,6 @@ class prepare_mmcif_files_for_deposition(QtCore.QThread):
             else:
                 self.data_template_dict['fragment_name_one_specific_mutation'] = '"' + mutations.replace(' ', '') + '"'
 
-            pdbx_funding_ordinal_one = self.data_template_dict['pdbx_funding_ordinal_one']
-            if pdbx_funding_ordinal_one.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
-                self.data_template_dict['pdbx_funding_ordinal_one'] = ''
-                self.data_template_dict['pdbx_funding_organization_one'] = ''
-                self.data_template_dict['pdbx_grant_number_one'] = ''
-                self.data_template_dict['pdbx_grant_country_one'] = ''
-
-            pdbx_funding_ordinal_two = self.data_template_dict['pdbx_funding_ordinal_two']
-            if pdbx_funding_ordinal_two.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
-                self.data_template_dict['pdbx_funding_ordinal_two'] = ''
-                self.data_template_dict['pdbx_funding_organization_two'] = ''
-                self.data_template_dict['pdbx_grant_number_two'] = ''
-                self.data_template_dict['pdbx_grant_country_two'] = ''
-
-            pdbx_funding_ordinal_three = self.data_template_dict['pdbx_funding_ordinal_three']
-            if pdbx_funding_ordinal_three.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
-                self.data_template_dict['pdbx_funding_ordinal_three'] = ''
-                self.data_template_dict['pdbx_funding_organization_three'] = ''
-                self.data_template_dict['pdbx_grant_number_three'] = ''
-                self.data_template_dict['pdbx_grant_country_three'] = ''
-
             # get protein chains
             self.data_template_dict['protein_chains'] = ''
             chains = self.pdb.GetProteinChains()
@@ -1154,6 +1128,44 @@ class prepare_mmcif_files_for_deposition(QtCore.QThread):
 #
 #            elif '_refine.ls_d_res_low' in line and len(line.split()) == 2:
 #                sys.stdout.write('_refine.ls_d_res_low                             {0!s}\n'.format(min(low_reso_list)))
+
+    def add_funding_information(self,xtal):
+        pdbx_funding_ordinal_one = self.data_template_dict['pdbx_funding_ordinal_one']
+        if pdbx_funding_ordinal_one.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
+            self.data_template_dict['pdbx_funding_ordinal_one'] = ''
+            self.data_template_dict['pdbx_funding_organization_one'] = ''
+            self.data_template_dict['pdbx_grant_number_one'] = ''
+            self.data_template_dict['pdbx_grant_country_one'] = ''
+
+        pdbx_funding_ordinal_two = self.data_template_dict['pdbx_funding_ordinal_two']
+        if pdbx_funding_ordinal_two.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
+            self.data_template_dict['pdbx_funding_ordinal_two'] = ''
+            self.data_template_dict['pdbx_funding_organization_two'] = ''
+            self.data_template_dict['pdbx_grant_number_two'] = ''
+            self.data_template_dict['pdbx_grant_country_two'] = ''
+
+        pdbx_funding_ordinal_three = self.data_template_dict['pdbx_funding_ordinal_three']
+        if pdbx_funding_ordinal_three.lower().replace(' ', '').replace('none', '').replace('null', '') == '':
+            self.data_template_dict['pdbx_funding_ordinal_three'] = ''
+            self.data_template_dict['pdbx_funding_organization_three'] = ''
+            self.data_template_dict['pdbx_grant_number_three'] = ''
+            self.data_template_dict['pdbx_grant_country_three'] = ''
+
+        funding_info = (
+            'loop\n'
+            '_pdbx_audit_support.ordinal                   \n'
+            '_pdbx_audit_support.funding_organization      \n'
+            '_pdbx_audit_support.grant_number              \n'
+            '_pdbx_audit_support.country                   \n'
+            "%s '%s' '%s' '%s'\n" %(depositDict['pdbx_funding_ordinal_one'],depositDict['pdbx_funding_organization_one'],depositDict['pdbx_grant_number_one'],depositDict['pdbx_grant_country_one']) +
+            "%s '%s' '%s' '%s'\n" %(depositDict['pdbx_funding_ordinal_two'],depositDict['pdbx_funding_organization_two'],depositDict['pdbx_grant_number_two'],depositDict['pdbx_grant_country_two']) +
+            "%s '%s' '%s' '%s'\n" %(depositDict['pdbx_funding_ordinal_three'],depositDict['pdbx_funding_organization_three'],depositDict['pdbx_grant_number_three'],depositDict['pdbx_grant_country_three']) +
+            '#\n'
+        )
+
+        f = open(xtal + '.mmcif', 'a')
+        f.write(funding_info)
+        f.close()
 
     def add_ligand_cif_to_model_mmcif(self,xtal):
         filestatus = False
