5e00fb2db0e68edbee85e777872b1c78357e94d5
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2022-06-16 14:59:51 +0200

    v1.9.1 (16/06/2022) - changed add instances of 'module load ccp4' to 'module load ccp4/7.1.018', except for the pandda2 function

--- XChemExplorer_dmd.sh
+++ XChemExplorer_dmd.sh
@@ -2,13 +2,11 @@
 
 if [ -d "/dls/labxchem" ]
   then
-    export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XChemExplorer_new/XChemExplorer"
-    source $XChemExplorer_DIR/setup-scripts/xce.setup-sh
-    module unload ccp4
-    source /dls/science/groups/i04-1/software/pandda_0.2.12/ccp4/ccp4-7.0/bin/ccp4.setup-sh
-    module load mx
-    export BDG_TOOL_MOGUL=/dls_sw/apps/ccdc/CSD_2020/bin/mogul
+    module load buster/20211020
+    module load phenix/1.20
     module load ccp4/7.1.018
+    export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XChemExplorer_new/XChemExplorer"
+    export BDG_TOOL_MOGUL=/dls_sw/apps/ccdc/CSD_2020/bin/mogul
 fi
 
 ccp4-python $XChemExplorer_DIR/XChemExplorer.py
