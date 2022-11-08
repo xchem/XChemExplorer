99ae3fbd78450f70bc96fed87fb59e81764ce3ee
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-11-08 14:26:40 +0000

    Clean up reset_interesting_datasets

--- xce/helpers/reset_interesting_datasets.py
+++ xce/helpers/reset_interesting_datasets.py
@@ -22,8 +22,6 @@ def check_interesting_datasets(panddaDir):
     os.chdir(os.path.join(panddaDir, "interesting_datasets"))
     for xtal in sorted(glob.glob("*")):
         if not os.path.islink(xtal):
-            #            print xtal
-            #            print os.path.join(panddaDir,'interesting_datasets',xtal,'modelled_structures',xtal+'-pandda-model.pdb')
             if os.path.isfile(
                 os.path.join(
                     panddaDir,
