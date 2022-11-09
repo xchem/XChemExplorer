ca55479535f5a097ca85dee911850d5e1d58eacb
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-11-09 17:26:14 +0000

    Setup dls run script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xce",
+    version="1.5.0",
+    description="GUI tool for XChem",
+    author="Tobias Krojer",
+    packages=find_packages(),
+    install_requires=[],
+)
+
