7e22f53676a850674278b0519b45bab23d72e99a
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-07-24 12:56:50 +0000

    Use cluster submission function for pandda2

--- xce/lib/cluster/sge.py
+++ xce/lib/cluster/sge.py
@@ -5,9 +5,27 @@ PROJECT_NAME = "labxchem"
 QUEUE = "medium.q"
 
 
-def submit_cluster_job(name, file, resources=""):
+def submit_cluster_job(
+    name, file, resources=None, parallel_environment=None, logfile=None, errfile=None
+):
+    base_command = "qsub -P {} -q {} -N {}".format(PROJECT_NAME, QUEUE, name)
+    resource_arg = "-l {}".format(resources) if resources is not None else ""
+    parallel_environment_arg = (
+        "-pe {}".format(parallel_environment)
+        if parallel_environment is not None
+        else ""
+    )
+    logfile_arg = "-o {}".format(logfile) if logfile is not None else ""
+    errfile_arg = "-e {}".format(errfile) if errfile is not None else ""
     os.system(
-        "qsub -P {} -q {} -N {} -l {} {}".format(
-            PROJECT_NAME, QUEUE, name, resources, file
+        " ".join(
+            [
+                base_command,
+                resource_arg,
+                parallel_environment_arg,
+                logfile_arg,
+                errfile_arg,
+                file,
+            ]
         )
     )
