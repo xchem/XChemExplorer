75bdb8c95cd233af35b6939a41e64a58aab42900
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-01-15 14:39:15 +0000

    Fix black errors

--- dls_launch.py
+++ dls_launch.py
@@ -3,4 +3,5 @@ import sys
 if __name__ == "__main__":
     sys.path.insert(0, "/dls_sw/i04-1/software/XChemExplorer/dist/xce-1.5.0-py2.7.egg")
     from xce.XChemExplorer import XChemExplorer
+
     XChemExplorer(sys.argv[1:])
