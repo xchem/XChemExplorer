c40f6092f18f4bc6efc66c4e3ac8f6eb3a91b8df
Daren-fearon <64987111+Daren-fearon@users.noreply.github.com>
2024-08-01 13:38:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="xce",
-    version="1.5.0",
+    version="2.0.1",
     description="GUI tool for XChem",
     author="Tobias Krojer",
     packages=find_packages(),
