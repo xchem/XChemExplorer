14bc596d6e1c745f988e51cce95aad3e5a6f1156
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-11-18 14:39:01 +0000

    Use most recent egg in launch script

--- launch.py
+++ launch.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     sys.path.insert(
-        0, os.path.join(os.environ["XChemExplorer_DIR"], "dist", "xce-1.5.0-py2.7.egg")
+        0, os.path.join(os.environ["XChemExplorer_DIR"], "dist", "xce-2.0.1-py2.7.egg")
     )
     from xce.XChemExplorer import XChemExplorer
 
